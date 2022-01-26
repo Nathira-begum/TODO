03c927d701c1335eb5520677fb5caa5af31cbd10
Nathira-begum <85860993+Nathira-begum@users.noreply.github.com>
2022-01-26 15:46:59 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import App from './App';
+import {name as appName} from './app.json';
+import "./assests/styles.css";
+
+AppRegistry.registerComponent(appName, () => App);
